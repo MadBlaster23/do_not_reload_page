899a6aa794f6811ad77b8fb9cb1c76d9c7714c48
MadBlaster23 <madblaster23@protonmail.com>
2019-03-10 10:57:43 -0700

    Updates

--- post_data.php
+++ post_data.php
@@ -7,8 +7,6 @@ $msg =
 "Message: ".$message;
 
     if(isset($_POST['user_name']) && isset($_POST['user_message'])) {
-        $name=$_POST['user_name'];
-        $message=$_POST['user_message'];
         mail("$admin", "Request", $msg);
     }
 ?>
