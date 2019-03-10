533fc82720a206cfc6bdb2235b9924c3405af60c
MadBlaster23 <madblaster23@protonmail.com>
2019-03-10 11:53:34 -0700

    Huge updates

--- post_data.js
+++ post_data.js
@@ -1,13 +1,28 @@
 function post() {
-    var name = document.getElementById("name").value;
-    var message = document.getElementById("message").value;
-    if(name && message) {
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var address1 = document.getElementById("address1").value;
+    var address2 = document.getElementById("address2").value;
+    var number = document.getElementById("number").value;
+    var date = document.getElementById("date").value;
+    var comments = document.getElementById("comments").value;
+
+    if(firstname && lastname && email && phone && address1 && address2 && number && date && comments) {
         $.ajax( {
             type: 'post',
             url: 'post_data.php',
             data: {
-                user_name:name,
-		        user_message:message
+                first_name:firstname,
+		        last_name:lastname,
+                user_email:email,
+                user_phone:phone,
+                user_address1:address1,
+                user_address2:address2,
+                user_number:number,
+                user_date:date,
+                user_comments:comments
             },
             success: function (response) {
 	            document.getElementById("status").innerHTML="Form Submitted Successfully";
--- post_data.php
+++ post_data.php
@@ -1,12 +1,26 @@
 <?php
 $admin = "evgeniusness@gmail.com";
-$name = $_REQUEST['user_name'];
-$message = $_REQUEST['user_message'];
+$firstname = $_REQUEST['first_name'];
+$lastname = $_REQUEST['last_name'];
+$email = $_REQUEST['user_email'];
+$phone = $_REQUEST['user_phone'];
+$address1 = $_REQUEST['user_address1'];
+$address2 = $_REQUEST['user_address2'];
+$number = $_REQUEST['user_number'];
+$date = $_REQUEST['user_date'];
+$comments = $_REQUEST['user_comments'];
 $msg =
-"Name: ".$name."\r\n".
-"Message: ".$message;
+"First Name: ".$firstname."\r\n".
+"Last Name: ".$lastname."\r\n".
+"Email: ".$email."\r\n".
+"Phone number: ".$phone."\r\n".
+"Pick-up address: ".$address1."\r\n" .
+"Delivery address: ".$address2."\r\n".
+"Number of rooms: ".$number."\r\n".
+"Moving date: ".$date."\r\n".
+"Any additional details: ".$comments;
 
-    if(isset($_POST['user_name']) && isset($_POST['user_message'])) {
-        mail("$admin", "Request", $msg);
-    }
+if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['user_email']) && isset($_POST['user_phone']) && isset($_POST['user_address1']) && isset($_POST['user_address2']) && isset($_POST['user_number']) && isset($_POST['user_date']) && isset($_POST['user_comments'])) {
+    mail("$admin", "Request", $msg);
+}
 ?>
