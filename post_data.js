2f986f462fb50ba3bbe3f86e83e73ce7252850a8
MadBlaster23 <madblaster23@protonmail.com>
2019-03-10 11:01:13 -0700

    Updates

--- post_data.js
+++ post_data.js
@@ -1,18 +1,18 @@
 function post() {
-            var name = document.getElementById("name").value;
-            var message = document.getElementById("message").value;
-            if(name && message) {
-                $.ajax( {
-                    type: 'post',
-                    url: 'post_data.php',
-                    data: {
-                        user_name:name,
-		                user_message:message
-                    },
-                    success: function (response) {
-	                    document.getElementById("status").innerHTML="Form Submitted Successfully";
-                    }
-                });
-             }
-            return false;
-        }
+    var name = document.getElementById("name").value;
+    var message = document.getElementById("message").value;
+    if(name && message) {
+        $.ajax( {
+            type: 'post',
+            url: 'post_data.php',
+            data: {
+                user_name:name,
+		        user_message:message
+            },
+            success: function (response) {
+	            document.getElementById("status").innerHTML="Form Submitted Successfully";
+            }
+        });
+    }
+    return false;
+}
