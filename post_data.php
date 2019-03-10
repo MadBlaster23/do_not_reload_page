711a64dcfc3c43fb03edee2a14cf46a66862986e
MadBlaster23 <madblaster23@protonmail.com>
2019-03-10 00:39:45 -0800

    PHP update

--- post_data.php
+++ post_data.php
@@ -1,10 +1,6 @@
 <?php
-
-if(isset($_POST['user_name']) && isset($_POST['user_message']))
-{
-  $name=$_POST['user_name'];
-  $message=$_POST['user_message'];
-
-}
-
+    if(isset($_POST['user_name']) && isset($_POST['user_message'])) {
+        $name=$_POST['user_name'];
+        $message=$_POST['user_message'];
+    }
 ?>
