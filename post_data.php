069d5cb103738e6dae6e3b5b83d1d2cd78e2303c
MadBlaster23 <madblaster23@protonmail.com>
2019-03-10 00:30:07 -0800

    Updates

---
+++ post_data.php
@@ -0,0 +1,10 @@
+<?php
+
+if(isset($_POST['user_name']) && isset($_POST['user_message']))
+{
+  $name=$_POST['user_name'];
+  $message=$_POST['user_message'];
+
+}
+
+?>
--- send.php
+++
@@ -1,5 +0,0 @@
-<?php
-$fname=$_POST['fname'];
-$lname=$_POST['lname'];
-echo "First name: ".$fname. "<br/>Last name: ".$lname;
-?>
