91b94bd7e803a6dbc277fda724aa004eacff7059
MadBlaster23 <madblaster23@protonmail.com>
2019-03-10 00:46:38 -0800

    Test

--- post_data.php
+++ post_data.php
@@ -1,6 +1,14 @@
 <?php
+$admin = "evgeniusness@gmail.com";
+$name = $_REQUEST['user_name'];
+$message = $_REQUEST['user_message'];
+$msg =
+"Name: ".$name."\r\n".
+"Message: ".$message;
+
     if(isset($_POST['user_name']) && isset($_POST['user_message'])) {
         $name=$_POST['user_name'];
         $message=$_POST['user_message'];
+        mail("$admin", "Request", $msg);
     }
 ?>
